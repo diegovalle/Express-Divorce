cf6bcfefe05dec1fc9ea68477d4cbe8882cbf506
Diego Valle-Jones <diegovalle@gmail.com>
2012-05-25 13:12:14 -0500

    fixed bug in addSource

--- src/functions-constants.R
+++ src/functions-constants.R
@@ -10,12 +10,12 @@ express.divorce.date <- as.Date("2008-10-06")
 
 
 addSource <- function(plot, text = "Data Source: Vital Statistics INEGI") {
-  p <- arrangeGrob(p, 
+  plot <- arrangeGrob(plot, 
               sub = textGrob(text,
                 x = 0, hjust = -0.1, vjust=0.1,
                 gp = gpar(fontface = "italic", fontsize = 9,
                   col = "gray50")))
-  return(p)
+  return(plot)
 }
 
 
